f689f0dce6099d164243512fba77497ea4d1df04
LucasSmielevski <lucassmielevski04@gmail.com>
2024-07-30 12:50:42 -0300

    adicionando exercicio 4 e 6

--- src/aula1/collections/Exercicio4.java
+++ src/aula1/collections/Exercicio4.java
@@ -1,9 +1,34 @@
 package aula1.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
 public class Exercicio4 {
 
 	public static void main(String[] args) {
 		new Exercicio4();
 	}
 
+	public Exercicio4() {
+		String frase = JOptionPane.showInputDialog("Digite uma frase:").toLowerCase();
+		Map<Character, Integer> contagemCaracteres = new HashMap<>();
+
+		for (int i = 0; i < frase.length(); i++) {
+			char letra = frase.charAt(i);
+			if (contagemCaracteres.containsKey(letra)) {
+				contagemCaracteres.put(letra, contagemCaracteres.get(letra) + 1);
+			} else {
+				contagemCaracteres.put(letra, 1);
+			}
+		}
+
+		String resultado = "Resultado:\n";
+		for (Map.Entry<Character, Integer> entry : contagemCaracteres.entrySet()) {
+			resultado += entry.getKey() + " - " + entry.getValue() + ", ";
+		}
+
+		JOptionPane.showMessageDialog(null, resultado, "Contagem de Caracteres", JOptionPane.INFORMATION_MESSAGE);
+	}
 }
--- src/aula1/collections/Exercicio6.java
+++ src/aula1/collections/Exercicio6.java
@@ -12,14 +12,29 @@ public class Exercicio6 {
 	}
 
 	public Exercicio6() {
-		Map<String, Double> alunos = new HashMap<String, Double>();
-		for (int i = 0; i < 2; i++) {
-			String nome = JOptionPane.showInputDialog("Digite o nome do aluno: " + (i+1));
+		Map<String, double[]> alunos = new HashMap<String, double[]>();
+		for (int i = 0; i < 5; i++) {
+			String nome = JOptionPane.showInputDialog("Digite o nome do aluno: " + (i + 1));
+			double[] notas = new double[3];
 			for (int j = 0; j < 3; j++) {
-				double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite as notas do aluno: "));
-				alunos.put(nome, nota);
+				double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite as notas do aluno " + nome));
+				notas[j] = nota;
 			}
+			alunos.put(nome, notas);
 		}
-		System.out.println(alunos);
+		for (Map.Entry<String, double[]> entry : alunos.entrySet()) {
+			String nome = entry.getKey();
+			double[] notas = entry.getValue();
+			double soma = 0;
+			String msg = "Notas do Aluno: " + nome;
+
+			for (double nota : notas) {
+				soma+= nota;
+				msg += "\nNota: " + nota;
+			}
+			double media = soma/notas.length;
+			String resultado = String.format("%.1f", media);
+			JOptionPane.showMessageDialog(null, msg + "\nMédia: " + resultado);
+		}
 	}
 }
