b67f0cbe8a62bd241e36dd5dd0118f750c515c48
LucasSmielevski <lucassmielevski04@gmail.com>
2024-07-31 18:36:36 -0300

    adicionando desafio de poo

--- src/aula2/poo/desafio/Carro.java
+++ src/aula2/poo/desafio/Carro.java
@@ -2,4 +2,16 @@ package aula2.poo.desafio;
 
 public class Carro {
 	
+	 String modelo;
+     int ano;
+    
+    public Carro(String modelo, int ano) {
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    
+    public Carro() {
+    	
+    }
+
 }
--- src/aula2/poo/desafio/Principal.java
+++ src/aula2/poo/desafio/Principal.java
@@ -1,9 +1,48 @@
 package aula2.poo.desafio;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		new Principal();
+	}
+
+	public Principal() {
+		List<Carro> carros = new ArrayList<>();
+
+		while (true) {
+			String modelo = JOptionPane.showInputDialog("Digite o modelo do carro (ou 'fim' para encerrar):");
+			if (modelo.equalsIgnoreCase("fim")) {
+				break;
+			}
+
+			String anoStr = JOptionPane.showInputDialog("Digite o ano do carro:");
+			int ano;
+			ano = Integer.parseInt(anoStr);
+
+			carros.add(new Carro(modelo, ano));
+		}
+
+		Collections.sort(carros, new Comparator<Carro>() {
+			@Override
+			public int compare(Carro c1, Carro c2) {
+				return Integer.compare(c1.ano, c2.ano);
+			}
+		});
 		
+
+		String resultado = "Carros cadastrados (do mais velho ao mais novo):\n";
+		for (Carro carro : carros) {
+			resultado += "Modelo: " + carro.modelo + ", Ano: " + carro.ano + "\n";
+		}
+
+		JOptionPane.showMessageDialog(null, resultado);
 	}
 
 }
