0dd096389e95abe48d13fa02dd2c2ab496f18905
LucasSmielevski <lucassmielevski04@gmail.com>
2024-08-01 18:32:11 -0300

    adicionando desafio exception completo

--- src/aula3/tratamentoserros/Desafio.java
+++ src/aula3/tratamentoserros/Desafio.java
@@ -16,32 +16,42 @@ public class Desafio {
 		venda.nomeCliente = JOptionPane.showInputDialog("Digite o nome do cliente: ");
 		JOptionPane.showMessageDialog(null, adicionarItens(venda));
 	}
-	
+
 	public static String adicionarItens(Venda venda) {
 		String msg = "";
-		int quantidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de itens: "));
+		int quantidade = 0;
 		try {
+			quantidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de itens: "));
+
 			if (quantidade < 1 || quantidade > 10) {
 				throw new VendaException("A quantidade de itens deve ser entre 1 e 10");
 			}
+			for (int i = 0; i < quantidade; i++) {
+				String nomeItem = JOptionPane.showInputDialog("Digite o nome do item " + (i + 1));
+				double precoItem = Double.parseDouble(JOptionPane.showInputDialog("Digite o preço do item " + (i + 1)));
+				try {
+					if (precoItem <= 0) {
+						throw new PrecoIncorretoException("O preço do item não pode ser menor que 1.");
+					} else {
+						Item item = new Item(nomeItem, precoItem);
+						venda.itens.add(item);
+					}
+				} catch (PrecoIncorretoException e) {
+					JOptionPane.showMessageDialog(null, "Erro no preço do item: " + e.getMessage(), "error", 0);
+					return adicionarItens(venda);
+				} catch (NullPointerException | NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "Valor inválido, digite novamente");
+					return adicionarItens(venda);
+				}
+			}
+			msg += "Venda criada com sucesso para o cliente " + venda.nomeCliente + " com " + venda.itens.size()
+					+ " itens";
+
 		} catch (VendaException e) {
 			JOptionPane.showMessageDialog(null, "Erro na venda: " + e.getMessage(), "error", 0);
-		}
-		for (int i = 0; i < quantidade; i++) {
-			String nomeItem = JOptionPane.showInputDialog("Digite o nome do item " + (i + 1));
-			double precoItem = Double.parseDouble(JOptionPane.showInputDialog("Digite o preço do item " + (i + 1)));
-			try {
-				if (precoItem <= 0) {
-					throw new PrecoIncorretoException("O preço do item não pode ser menor que 1.");
-				} else {
-					Item item = new Item(nomeItem, precoItem);
-					venda.itens.add(item);
-					msg += "Venda criada com sucesso para o cliente " + venda.nomeCliente + " com " + venda.itens.size() + " itens";
-				}
-			} catch (PrecoIncorretoException e) {
-				JOptionPane.showMessageDialog(null, "Erro no preço do item: " + e.getMessage(), "error", 0);
-				return adicionarItens(venda);
-			}
+		} catch (NumberFormatException | NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Valor inválido, digite novamente");
+			return adicionarItens(venda);
 		}
 		return msg;
 
